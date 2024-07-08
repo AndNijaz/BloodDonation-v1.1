0a1cb40bd1835473a3ea69a9587b132c8e9d62c4
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2024-07-08 21:00:13 +0200

    commit

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,8 +1,12 @@
 import { Redirect } from "expo-router";
 import { useAuth } from "../context/AuthProvider";
 
+import { supabase } from "@/lib/supabase";
+
 export default function index() {
   const { session } = useAuth();
+
+  // supabase.auth.signOut();
 
   if (session) return <Redirect href="/" />;
 
